a82a7b2d06b7f2e5cc7ce83129c5a64aae7e109b
Gilberto Afonso Francisco <gafrancisco@gmail.com>
2021-10-09 14:28:03 +0100

    some refactoring ....

--- Calculator.cs
+++ Calculator.cs
@@ -16,8 +16,8 @@ namespace NPF_Teste
         /// Caso o resultado seja igual a 0, pode-se afirmar também que é divisível por 11.
         /// 
         /// </summary>
-        /// <param name="numbers"></param>
-        /// <returns></returns>
+        /// <param name="numbers">String numbers List</param>
+        /// <returns>List numbers/multiplicity</returns>
         public static List<Result> IsMultipleOf11(List<string> numbers)
         {
             List<Result> result = new List<Result>(); // result list
@@ -25,34 +25,33 @@ namespace NPF_Teste
             foreach (string n in numbers) // para cada item na lista de entrada
             {
                 Result itemResult = new Result() { number = n, isMultiple = false }; // by default
-                
+
                 string numero = n.Trim(); // trim dos espaços
                 int size = numero.Length; //tamanho da string
                 int sums = 0;
 
                 try
                 {
-                    /******* para numeros pequenos (dois digitos) devolve por este metedo ***********/
-                    if (size <= 2) 
+                    if (size > 2) /******* para numeros maiores ********/
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            int val = int.Parse(numero[i].ToString());
+
+                            // soma e substrai respectivamente os digitos na posicao par e impar
+                            sums += ((i + 1) % 2 == 0) ? val : -val;
+                        }
+
+                        int resto = sums % 11; //resto das somas e subtraccoes por 11
+
+                        itemResult.isMultiple = (resto == 0); //se o resto da divisao for igual a zero, entao é multiplo de 11
+                    }
+                    else /******* para numeros pequenos (dois digitos) ********/
                     {
                         itemResult.isMultiple = (int.Parse(numero) % 11) == 0;
-                        result.Add(itemResult);
-                        continue;
                     }
 
-                    /******* para numeros maiores devolve por este metedo ***********/
-                    for (int i = 0; i < size; i++)
-                    {
-                        int val = int.Parse(numero[i].ToString());
-
-                        // soma e substrai respectivamente os digitos na posicao par e impar
-                        sums += ((i + 1) % 2 == 0) ? val : -val; 
-                    }
-
-                    int resto = sums % 11; //resto das somas e subtraccoes por 11
-
-                    itemResult.isMultiple = (resto == 0); //se o resto da divisao for igual a zero, entao é multiplo de 11
-                    result.Add(itemResult); 
+                    result.Add(itemResult);
                 }
                 catch
                 {
